8eaa1d7b0bf45a3ad3fd6eabaa4b925e9117ca48
Vishal Patil <94173908+Vishalsayz@users.noreply.github.com>
2024-10-05 17:49:33 +0530

    Add files via upload

---
+++ P12_MethodOverloading/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            // Method overloading - Intro
+
+            Intro("Swati", "Jathar", 22); 
+
+            Intro("Pradip Jathar", 24);               // Method overload - Rule 1 - Change the number of parameter.
+
+            Intro(27, "Pravin Gunjal");               // Method overload - Rule 2 - Change the position of parameter.
+
+            Intro(23.5, "Shubhangi Gunjal");          // Method overload - Rule 3 - Change the datatype of parameter. --> (if you required same position).
+        }
+
+
+        // Method (void Method - Method with no return value)
+
+        // Method overload
+        static void Intro(string firstName, string lastName, int age)
+        {
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine("Hii,");
+            Console.WriteLine($"My name is {fullName}.");
+            Console.WriteLine($"My age is {age}.\n");
+        }
+
+        // Method overload - Rule 1 - Change the number of parameter.
+        static void Intro(string fullName, int age)
+        {
+            Console.WriteLine("Rule 1 - Change the number of parameter.");
+            Console.WriteLine("Hii,");
+            Console.WriteLine($"My name is {fullName}.");
+            Console.WriteLine($"My age is {age}.\n");
+        }
+
+        // Method overload - Rule 2 - Change the position of parameter.
+        static void Intro(int age, string fullName)
+        {
+            Console.WriteLine("Rule 2 - Change the position of parameter.");
+            Console.WriteLine("Hii,");
+            Console.WriteLine($"My name is {fullName}.");
+            Console.WriteLine($"My age is {age}.\n");
+        }
+
+        // Method overload - Rule 3 - Change the datatype of parameter if you required same position.
+        static void Intro(double age, string fullName)
+        {
+            Console.WriteLine("Rule 3 - Change the datatype of parameter if you required same position.");
+            Console.WriteLine("Hii,");
+            Console.WriteLine($"My name is {fullName}.");
+            Console.WriteLine($"My age is {age}.\n");
+        }
+
+    }
+}
---
+++ P14_Constructors/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P14_Constructors
+{
+    // Constructor
+    // 1. Constructor is a special method that is used to initialize (create) objects.
+    // 2. Constructor always called when the object is created.
+    // 3. Constructor can be used to set initial values for fields.
+    // 4. Constructor name must match the class name.
+    // 5. Constructor cannot have a return type (like void or int).
+    // 6. Constructors can also take parameters, which is used to initialize fields.
+    // 7. Just like other methods, constructors can be overloaded by using different numbers of parameters.
+    // 8. All classes have constructors by default: if you do not create a class constructor yourself, C# creates one for you.
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car ford = new Car("Figo", "Black", 2010, 1016);                // 1. Constructor is a special method that is used to initialize (create) objects.
+                                                                            // 2. Constructor always called when the object is created.
+
+            Console.WriteLine($"{ford.model} - {ford.color} - {ford.year} - {ford.weight} kg");
+
+
+            Car tata = new Car("Safari", 2017);                             // Car constructor 2nd overload.
+
+            Console.WriteLine($"{tata.model} - {tata.year}");
+
+            Console.WriteLine();
+        }
+    }
+
+  
+
+    class Car
+    {
+        public string model;            // Fields
+
+        public string color;            
+
+        public int year;                
+
+        public int weight;
+
+
+        public Car(string modelName, string modelColor, int modelYear, int modelWeight)     // 4. Constructor name must match the class name.
+        {                                                                                   // 5. Constructor cannot have a return type (like void or int).
+            model = modelName;                                                              
+            color = modelColor;         
+            year = modelYear;           
+            weight = modelWeight;                                                           // 3. Constructor can be used to set initial values for fields.
+        }   
+        
+        public Car(string modelName, int modelYear)                                         // 7. Just like other methods, constructors can be overloaded by using different numbers of parameters.
+        {
+            model = modelName;
+            year = modelYear;
+        }
+
+    }
+}
